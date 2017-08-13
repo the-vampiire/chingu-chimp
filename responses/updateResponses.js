5aee8a6dd6284478cf1d7e9b7424e22f368a74c9
Patrick Cleary <vampiirecodes@gmail.com>
2017-08-12 22:25:48 -0400

    changed update url help response to be attachment based

--- responses/updateResponses.js
+++ responses/updateResponses.js
@@ -55,83 +55,6 @@ submitSkill = valueObject => {
 
 helpResponse = (type) => {
 
-    // const help1 =
-    //     `*How to use the \`/update\` command:* \n\nUpdating functions similarly to git in stringing together mandatory and/or optional \`[-flag data]\` pairs to build the update command\n
-    //     *General form: \`/update [profile item] [-flag data]\`*\n
-    //     *List of update items: \`blog\`, \`certifications\`, \`gitHub\`, \`picture\`, \`portfolio\`, \`projects\`, \`skills\`, \`story\`*\n
-    //     *List of update flags: \`-date\` or \`-d\`, \`-git\` or \`-g\`, \`-name\` or \`-d\`, \`-url\` or \`-u\`*\n
-    //
-    //     *Updating GitHub, Blog, or Portfolio URLs*
-    //     \t*Item(s)*
-    //     \t\t[\`gitHub\`]: your gitHub profile url
-    //     \t\t[\`blog\`]: your blog url
-    //     \t\t[\`portfolio\`]: your portfolio url
-    //     \t*Flag(s)*
-    //     \t\t[\`-url\`\`full url\`] _example:_ \`-url https://www.github.com/yourUserName\`
-    //
-    //     \t_example update of blog url:_ \`/update blog -url https://medium.com/@yourUserName\`
-    //     \t_example *shorthand* update of blog url:_ \`/update blog -u https://medium.com/@yourUserName\`
-    //     \n
-    //     *Updating User Story*
-    //     \t*Item(s)*
-    //     \t\t[\`story\`]: your user story (that you entered in the Intro channel)
-    //     \t*Flag(s)*
-    //     \t\tNone.
-    //     \t\t*Note:* You can preserve the formatting [markdown] by copying directly from the edit message window in your intro post
-    //
-    //     \t_example update of user story:_ \`/update story Hello you can call me Vamp I am a...\`
-    //     \n
-    //     *Adding Projects*
-    //     \t*Item(s)*
-    //     \t\t[\`projects\`]
-    //     \t*Flag(s)*
-    //     \t\t[\`-name\`\`project name\`] _example:_ \`-name Project Name\`
-    //     \t\t[\`-url\`\`project homepage\`] _example:_ \`-url https://www.domain.com/projectName\`
-    //     \t\t\t *Optional*: including a project url is recommended but not required
-    //     \t\t[\`-git\`\`project GitHub repo\`] _example:_ \`-git https://www.github.com/yourUserName/projectName\`
-    //     \t\t\t*Note*: a gitHub repo link is required for every project
-    //     \t\t[\`-date\`\`date of completion\`] _example:_ \`-date 01/01/17\`
-    //     \t\t\t *Optional*: If no date is passed - today's date is inserted.
-    //     \t\t\t *Note:* Date must be mm/dd/yy format.
-    //
-    //     \t_example adding a project:_
-    //     \t\t\`/update projects -name New Project -url https://www.domain.com/newProject\`
-    //     \t\t\`-git https://www.github.com/yourUserName/newProject -date 08/08/17\`
-    //     \t_example *shorthand* adding a project:_
-    //     \t\t\`/update projects -n New Project -u https://www.domain.com/newProject\`
-    //     \t\t\`-g https://www.github.com/yourUserName/newProject -d 08/08/17\`
-    //     \n
-    //     *Adding or Updating Skills*
-    //     \t*Item(s)*
-    //     \t\t[\`skills\`]: your languages and frameworks and their associated skill levels
-    //     \t*Flag(s)*
-    //     \t\tNone.
-    //     \tAn interactive form will be sent back to you to add or update a language or framework skill.
-    //     \t\t*Note*: to update an existing skill level select the language or framework then select the new skill level.
-    //     \n
-    //     *Adding Free Code Camp Certifications*
-    //     \t*Item(s)*
-    //     \t\t[\`certifications\`]: your Free Code Camp certifications
-    //     \t*Flag(s)*
-    //     \t\t[\`-url\`\`certificate url\`] _example:_ \`-url https://www.freecodecamp.org/fccUserName/front-end-certification\`
-    //     \t\t[\`-date\`\`date of completion\`] _example:_ \`-date 01/01/17\`
-    //     \t\t\t *Optional*: If no date is passed - today's date is inserted.
-    //     \t\t\t *Note:* Date must be mm/dd/yy format.
-    //
-    //     \t_example adding a new certificate:_
-    //     \t\t\`/update certifications -url https://www.freecodecamp.org/fccUserName/front-end-certification -date 08/08/17\`
-    //     \t_example *shorthand* adding a new certificate:_
-    //     \t\t\`/update certifications -u https://www.freecodecamp.org/fccUserName/front-end-certification -d 08/08/17\`
-    //
-    //     *Updating Your Profile Picture*
-    //     \t*Item(s)*
-    //     \t\t[\`picture\`]: the profile picture that is displayed on your profile card
-    //     \t*Flag(s)*
-    //     \t\tNone.
-    //     \tUse the command \`/update picture\` and your current Slack profile picture will be added to your profile card.
-    //
-    //     *if you need more help, have suggestions for improvement, or want to report a bug please add an issue on <https://www.github.com/the-vampiire/Chingu-Chimp/issues|GitHub>*`;
-
     const help1 = `*How to use the \`/update\` command:* \n\nUpdating functions similarly to git in stringing together mandatory and/or optional \`[-flag data]\` pairs to build the update command\n
         *General form: \`/update [profile item] [-flag data]\`*\n
         *List of update items: \`blog\`, \`certifications\`, \`gitHub\`, \`picture\`, \`portfolio\`, \`projects\`, \`skills\`, \`story\`*\n
@@ -210,7 +133,7 @@ helpResponse = (type) => {
                     },
                     {
                         title: 'Flag(s)',
-                        value: `None.\nNote: You can preserve the formatting [markdown] by copying directly from the edit message window in your intro post`,
+                        value: `None.\nNote: Your current Slack profile picture will be attached to your Chingu Profile Card`,
                     },
                     {
                         title: 'Example - updating your profile card picture',
@@ -252,18 +175,60 @@ helpResponse = (type) => {
         ]
     };
 
-    const url = `*General form: \`/update [profile item] [-flag data]\`*\n
-        *Updating GitHub, Blog, or Portfolio URLs*
-        \t*Item(s)*
-        \t\t[\`gitHub\`]: your gitHub profile url
-        \t\t[\`blog\`]: your blog url
-        \t\t[\`portfolio\`]: your portfolio url
+    const url = {
+
+        response_type: 'ephemeral',
+        text: '*Updating GitHub, Blog, or Portfolio URLs*',
+        attachments: [
+            {
+                mrkdwn_in: ['text', 'pretext'],
+                color: '#15df89',
+                pretext: '*General form: \`/update [profile item] [-flag data]\`',
+                text: `*Item(s)*\n\t
         \t*Flag(s)*
-        \t\t[\`-url\`\`full url\`] _example:_ \`-url https://www.github.com/yourUserName\`
-        
-        \t_example update of blog url:_ \`/update blog -url https://medium.com/@yourUserName\`
-        \t_example *shorthand* update of blog url:_ \`/update blog -u https://medium.com/@yourUserName\`
-        \n`;
+        \t\t`
+
+            },
+            {
+                mrkdwn_in: ['text', 'pretext'],
+                color: '#15df89',
+                pretext: '*Item(s)*',
+                fields: [
+                    {
+                        title: 'blog',
+                        value: 'your blog url'
+                    },
+                    {
+                        title: 'gitHub',
+                        value: 'your gitHub profile url',
+                        short: true
+                    },
+                    {
+                        title: 'Note',
+                        value: 'must begin with "https://www.github.com/"',
+                        short: true
+                    },
+                    {
+                        title: 'portfolio',
+                        value: 'your portfolio page url'
+                    }
+                ]
+            },
+            {
+                mrkdwn_in: ['text', 'pretext'],
+                color: '#15df89',
+                pretext: '*Flag(s)*',
+                text: `[\`-url\`\`full url\`] _example:_ \`-url https://www.github.com/yourUserName\``
+            },
+            {
+                mrkdwn_in: ['text', 'pretext'],
+                color: '#15df89',
+                pretext: '*Examples*',
+                text: `*_example update of blog url:_* \`/update blog -url https://medium.com/@yourUserName\`\n*_example *shorthand* update of blog url:_* \`/update gitHub -u https://github.com/yourUserName\``
+            }
+        ]
+
+    };
 
     const projects = `*General form: \`/update [profile item] [-flag data]\`*\n
         *Adding Projects*
