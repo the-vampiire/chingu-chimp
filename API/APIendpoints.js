e8029d793c4df03ad41bc5a177d2cb09de19a078
Patrick Cleary <vampiirecodes@gmail.com>
2017-08-17 13:28:18 -0400

    beginning work on the PUT request (updating the database) endpoint for the Chingu site API

--- API/APIendpoints.js
+++ API/APIendpoints.js
@@ -2,26 +2,28 @@
 const express = require('express');
 const router = module.exports = express.Router();
 
-// GET requests 
+const verified = require('./tools/verify');
+
+// GET [request data from the database] 
     // bulk users bulk items
     // bulk users one item
     // one user bulk items
     // one user one item
-router.get('/item', (req, res) => {
+router.get('/request', (req, res) => {
 
-    const GET = require('./getMethods');
+    const GET = require('./tools/getMethods');
 
     const request = req.query;
     let error = {};
     
-// accepted request content 
+    // accepted request content 
     const chinguAPIKey = request.key;
     const userName = request.userName;
     const profileItem = request.profileItem;
     const bulkItems = request.bulkItems;
     const bulkUsers = request.bulkUsers;
 
-    if(chinguAPIKey === 'test'){
+    if(verified(chinguAPIKey)){
     
     // bulk users
         if(bulkUsers){  
@@ -70,6 +72,18 @@ router.get('/item', (req, res) => {
     if(error.error) res.json(error);
 });
 
+
+// PUT [update data in the database]
+router.put('/update', (req, res) => {
+
+    const request = req.body;
+    const chinguAPIKey = request.key;
+
+    if(verified(chinguAPIKey)){
+        
+    }
+});
+
 // errror handler
 APIerror = (type, error, request, mongoError) => {
 
@@ -97,44 +111,44 @@ APIerror = (type, error, request, mongoError) => {
 
 // // TESTING
 // router.get('/', (req, res) => {
-//     const request = require('request');
-//     const queries = req.query;
+    //     const request = require('request');
+    //     const queries = req.query;
 
-// test get one user one item
+    // test get one user one item
+        // request({
+        //    uri: 'https://65974b37.ngrok.io/API/item',
+        //    method: 'GET',
+        //    qs: { key: 'test', userName:'vampiire', profileItem: 'projects' },
+        // }, (err, returned) => {
+        //     res.json(JSON.parse(returned.body));
+        // });
+
+    // // test get one user with bulk items
+    //     request({
+    //        uri: 'https://65974b37.ngrok.io/API/item',
+    //        method: 'GET',
+    //        qs: { key: 'test', userName:'vampiire', bulkItems: ['blog', 'projects', 'portfolio'] },
+    //     }, (err, returned) => {
+    //         res.json(JSON.parse(returned.body));
+    //     });
+
+    // // test get bulk users with one item
+    //     request({
+    //        uri: 'https://65974b37.ngrok.io/API/item',
+    //        method: 'GET',
+    //        qs: { key: 'test', bulkUsers: ['vampiire', 'chance'], profileItem: 'blog' },
+    //     }, (err, returned) => {
+    //         res.json(JSON.parse(returned.body));
+    //     });
+
+    // // test get bulk users with bulk items
     // request({
-    //    uri: 'https://65974b37.ngrok.io/API/item',
-    //    method: 'GET',
-    //    qs: { key: 'test', userName:'vampiire', profileItem: 'projects' },
-    // }, (err, returned) => {
-    //     res.json(JSON.parse(returned.body));
-    // });
-
-// // test get one user with bulk items
-//     request({
-//        uri: 'https://65974b37.ngrok.io/API/item',
-//        method: 'GET',
-//        qs: { key: 'test', userName:'vampiire', bulkItems: ['blog', 'projects', 'portfolio'] },
-//     }, (err, returned) => {
-//         res.json(JSON.parse(returned.body));
-//     });
-
-// // test get bulk users with one item
-//     request({
-//        uri: 'https://65974b37.ngrok.io/API/item',
-//        method: 'GET',
-//        qs: { key: 'test', bulkUsers: ['vampiire', 'chance'], profileItem: 'blog' },
-//     }, (err, returned) => {
-//         res.json(JSON.parse(returned.body));
-//     });
-
-// // test get bulk users with bulk items
-// request({
-//     uri: 'https://65974b37.ngrok.io/API/item', method: 'GET',
-//     qs: { key: 'test', bulkUsers: ['vampiire', 'dsegovia90', 'chance', 'jessec'], 
-//     bulkItems: ['blog', 'bestStreak', 'skills'] }}, 
-    
-//     (err, returned) => {
-//         res.json(JSON.parse(returned.body));
-//  });
+    //     uri: 'https://65974b37.ngrok.io/API/item', method: 'GET',
+    //     qs: { key: 'test', bulkUsers: ['vampiire', 'dsegovia90', 'chance', 'jessec'], 
+    //     bulkItems: ['blog', 'bestStreak', 'skills'] }}, 
+        
+    //     (err, returned) => {
+    //         res.json(JSON.parse(returned.body));
+    //  });
 
 // });
